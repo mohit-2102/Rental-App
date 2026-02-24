c14a42d71dac5f6b4646219eea2a851577a371e4
mohit-2102 <mohit.mathangi@gmail.com>
2026-02-24 12:23:58 +0530

    Properly track client folder

---
+++ client/src/app/(dashboard)/managers/settings/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import SettingsForm from "@/components/SettingsForm";
+import {
+  useGetAuthUserQuery,
+  useUpdateManagerSettingsMutation,
+} from "@/state/api";
+import React from "react";
+
+const ManagerSettings = () => {
+  const { data: authUser, isLoading } = useGetAuthUserQuery();
+  const [updateManager] = useUpdateManagerSettingsMutation();
+
+  if (isLoading) return <>Loading...</>;
+  
+  if (!authUser || !authUser.userInfo) {
+    return <>Error: Unable to load user information</>;
+  }
+
+  const initialData = {
+    name: authUser.userInfo.name,
+    email: authUser.userInfo.email,
+    phoneNumber: authUser.userInfo.phoneNumber,
+  };
+
+  const handleSubmit = async (data: typeof initialData) => {
+    await updateManager({
+      cognitoId: authUser?.cognitoInfo?.userId,
+      ...data,
+    });
+  };
+
+  return (
+    <SettingsForm
+      initialData={initialData}
+      onSubmit={handleSubmit}
+      userType="manager"
+    />
+  );
+};
+
+export default ManagerSettings;
---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  console.log("test");
+  return (
+    <div>
+      Home
+    </div>
+  );
+}
